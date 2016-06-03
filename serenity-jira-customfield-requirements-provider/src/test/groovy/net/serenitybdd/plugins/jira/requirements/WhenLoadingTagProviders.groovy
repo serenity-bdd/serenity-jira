295090c6e5447b3ada183fe2db5847b218f864a1
YamStranger <YamStranger@bk.ru>
2016-06-03 19:38:03 +0300

    fix: fixed loading of JiraCustomFieldRequirementsProvider, added test

---
+++ serenity-jira-customfield-requirements-provider/src/test/groovy/net/serenitybdd/plugins/jira/requirements/WhenLoadingTagProviders.groovy
@@ -0,0 +1,19 @@
+package net.serenitybdd.plugins.jira.requirements
+
+import net.thucydides.core.statistics.service.ClasspathTagProviderService
+import net.serenitybdd.plugins.jira.requirements.JIRACustomFieldsRequirementsProvider
+import spock.lang.Specification
+
+class WhenLoadingTagProviders extends Specification {
+
+    def "Custom fields requirement provider can be loaded"() {
+        given: "classpath tag provider service"
+            def ClasspathTagProviderService service = new ClasspathTagProviderService();
+        when: "loading available tag providers"
+            def providers = service.getTagProviders();
+        then: "JIRACustomFieldsRequirementsProvider should be loaded"
+            providers.
+                findAll ({{provider -> provider instanceof JIRACustomFieldsRequirementsProvider}}).
+                size() == 3
+    }
+}
